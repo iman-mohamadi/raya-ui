5b698aff19c72701ce5c0810b1448882384a57c8
EnzO <im.enzo.021@gmail.com>
2025-12-31 14:14:54 +0330

    Update LiquidGlassDemo.vue

--- app/components/demo/LiquidGlassDemo.vue
+++ app/components/demo/LiquidGlassDemo.vue
@@ -3,8 +3,8 @@ import { LiquidGlass } from '@/components/ui/liquid-glass'
 </script>
 
 <template>
-  <div class="flex items-center justify-center w-full h-full p-8">
-    <LiquidGlass class="w-full h-full flex items-center justify-center text-xs font-bold text-white/80">
+  <div class="flex items-center justify-center w-full h-full bg-radial-[at_50%_75%] from-cyan-700 rounded-2xl to-blue-800 p-8">
+    <LiquidGlass class=" flex items-center justify-center text-xs font-bold text-white/80" containerClass="w-20 h-13">
       Glass
     </LiquidGlass>
   </div>
