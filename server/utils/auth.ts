bc88d1590cfd54ada2b6d50b31f93dcfc69fc15d
EnzO <im.enzo.021@gmail.com>
2026-01-06 16:20:02 +0330

    Update auth.ts

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -4,13 +4,22 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
 import * as schema from "../db/schema";
 
+// server/utils/auth.ts
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg",
         schema: { ...schema }
     }),
-    emailAndPassword: { enabled: true },
 
+    baseURL: process.env.BETTER_AUTH_URL,
+
+    trustedOrigins: [
+        "https://raya-ui.com",
+        "https://www.raya-ui.com",
+        "http://localhost:3000"
+    ],
+
+    emailAndPassword: { enabled: true },
     user: {
         additionalFields: {
             role: {
