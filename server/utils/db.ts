81793c7219b6d514e91addc2ce14478f98fb4f6c
EnzO <im.enzo.021@gmail.com>
2026-01-06 11:32:13 +0330

    Update db.ts

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -2,10 +2,6 @@
 import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 
-// 1. Create a "Stateless" HTTP connection
-// This sends one standard HTTPS request per query,
-// which is much harder for the Firewall to block than a WebSocket.
 const sql = neon(process.env.POSTGRES_URL!)
 
-// 2. Initialize Drizzle
 export const db = drizzle(sql)
